562c6ba78668501b5446b4b2c5a3625a6f3ca2cb
Robert Adkins <robertadkins00@gmail.com>
2015-09-20 05:08:07 -0400

    categorization code
    first running attempt at actually categorizing articles

---
+++ backend/driver.py
@@ -0,0 +1,27 @@
+from categorizer import *
+from scraper import *
+import json
+from pprint import pprint        
+
+if __name__ == "__main__":
+    f1 = open('lib_training_json.txt')
+    f2 = open('con_training_json.txt')
+    uniqlibart = json.load(f1)
+    uniqconart = json.load(f2)
+    #print len(libart)
+    #print len(conart)
+    #uniqlibart = list(set(libart))
+    #uniqconart = list(set(conart))
+    print len(uniqlibart)
+    print len(uniqconart)
+    #pprint(uniqlibart[0:10])
+    pprint(uniqlibart[0])
+    pprint(uniqlibart[1])
+    libavg = avg_article_topics(uniqlibart,'political_bias')
+    conavg = avg_article_topics(uniqconart,'political_bias')
+    cats = [('liberal',libavg),('conservative',conavg)]
+    for art in uniqlibart:
+        print categorize_article(art,cats,'political_bias')
+    print '\n\n\n\n'
+    for art in uniqconart:
+        print categorize_article(art,cats,'political_bias')
