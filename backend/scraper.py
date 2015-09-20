4e67bf11efdf7833e8c1dc6fdd87401123b4aad6
Jonah Chazan <person.1123.loztp@gmail.com>
2015-09-19 20:06:12 -0400

    New scraper is actually here

--- backend/scraper.py
+++ backend/scraper.py
@@ -1,9 +1,8 @@
 import urllib2, json
+import newspaper
 
-
-def scrapeReddit(term, recentness="week", limit=100,sort="hot"):
-    url = 'http://www.reddit.com/search.json?q=' + term + '&t=' + recentness + '&limit=' + str(limit) + '&sort=' + sort
-    print url
+def getRedditUrls(term, recentness="week", limit=100,sort="hot"):
+    url = 'http://www.reddit.com/search.json?q=' + urllib2.quote(term, safe='') + '&t=' + recentness + '&limit=' + str(limit) + '&sort=' + sort
     response = urllib2.urlopen(url)
     json_result = response.read()
 
@@ -13,6 +12,61 @@ def scrapeReddit(term, recentness="week", limit=100,sort="hot"):
 
     return urls
 
+def getGoogleNewsUrls(term, pages=3):
+    urls = []
+
+    base_url = 'https://ajax.googleapis.com/ajax/services/search/news?v=1.0&q=' + urllib2.quote(term, safe='')
+
+    for i in range(pages):
+        url = base_url + "&start=" + str(len(urls))
+        response = urllib2.urlopen(url)
+        json_result = response.read()
+
+        try:
+            result = json.loads(json_result)["responseData"]
+        
+            posts = result["results"]
+
+            urls += [post["unescapedUrl"] for post in posts]
+        except Exception:
+            print json_result
+            break
+    return urls
+
+
+def filterArticleUrls(urls):
+    blacklist = ["imgur.com","gfycat.com", "youtube.com"]
+    extBlacklist = ["png", "jpg", "gif", "gifv"]
+    matched = []
+    for url in urls:
+        parts = url.split("://")[1].split("/")[0]
+
+        if any([b in parts for b in blacklist]):
+            continue
+
+        ext = url.split(".")[-1]
+        if ext in extBlacklist:
+            continue
+        
+        yield url
+
+
+def getArticleInfos(urls):
+    for url in urls:
+        article = newspaper.Article(url)
+        article.download()
+        article.parse()
+
+        yield article
+                        
+
 
 if __name__ == "__main__":
-    print scrapeReddit("trump")
+    urls = filterArticleUrls(getGoogleNewsUrls("trump"))
+    articles = getArticleInfos(urls)
+
+    
+    for art in articles:
+        print art.text
+        break
+    
