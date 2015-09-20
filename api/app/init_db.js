5de65c73ae93b4c3a562c20c45f77bbeb95b1895
Aria Fallah <aria@vps.server.com>
2015-09-20 11:37:10 +0100

    before pull

--- api/app/init_db.js
+++ api/app/init_db.js
@@ -22,11 +22,11 @@
 
     DBModel.Category.sync({force: true}).then(() => {
       let categories = [
-        { name: 'Category 1', topicId: '1' },
-        { name: 'Category 2', topicId: '1' },
-        { name: 'Category 3', topicId: '1' },
-        { name: 'Category 1', topicId: '2' },
-        { name: 'Category 2', topicId: '2' },
+        { name: 'Category 1', topicId: '5' },
+        { name: 'Category 2', topicId: '5' },
+        { name: 'Category 3', topicId: '5' },
+        { name: 'Category 1', topicId: '4' },
+        { name: 'Category 2', topicId: '4' },
       ];
       
       for (let i = 0; i < categories.length; i++) {
@@ -36,19 +36,19 @@
       
       DBModel.Article.sync({force: true}).then(() => {
         let articles = [
-          { name: 'Article 1', summary: 'Summary 1', categoryId: 1 },
-          { name: 'Article 2', summary: 'Summary 2', categoryId: 1 },
-          { name: 'Article 3', summary: 'Summary 3', categoryId: 5 },
-          { name: 'Article 4', summary: 'Summary 4', categoryId: 4 },
-          { name: 'Article 5', summary: 'Summary 5', categoryId: 4 },
-          { name: 'Article 6', summary: 'Summary 6', categoryId: 5 },
-          { name: 'Article 7', summary: 'Summary 7', categoryId: 2 },
-          { name: 'Article 8', summary: 'Summary 8', categoryId: 2 },
-          { name: 'Article 9', summary: 'Summary 9', categoryId: 2 },
-          { name: 'Article 10', summary: 'Summary 10', categoryId: 3 },
-          { name: 'Article 11', summary: 'Summary 11', categoryId: 3 },
-          { name: 'Article 12', summary: 'Summary 12', categoryId: 3 },
-          { name: 'Article 13', summary: 'Summary 13', categoryId: 3 }
+          { name: 'Article 1', link: 'http://google.com', categoryId: 1 },
+          { name: 'Article 2', link: 'http://google.com', categoryId: 1 },
+          { name: 'Article 3', link: 'http://google.com', categoryId: 5 },
+          { name: 'Article 4', link: 'http://google.com', categoryId: 4 },
+          { name: 'Article 5', link: 'http://google.com', categoryId: 4 },
+          { name: 'Article 6', link: 'http://google.com', categoryId: 5 },
+          { name: 'Article 7', link: 'http://google.com', categoryId: 2 },
+          { name: 'Article 8', link: 'http://google.com', categoryId: 2 },
+          { name: 'Article 9', link: 'http://google.com', categoryId: 2 },
+          { name: 'Article 10', link: 'http://google.com', categoryId: 3 },
+          { name: 'Article 11', link: 'http://google.com', categoryId: 3 },
+          { name: 'Article 12', link: 'http://google.com', categoryId: 3 },
+          { name: 'Article 13', link: 'http://google.com', categoryId: 3 }
         ];
 
         for (let i = 0; i < articles.length; i++) {
--- perspective/app/models/article.js
+++ perspective/app/models/article.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  summary: DS.attr('string'),
+  link: DS.attr('string'),
   categoryId: DS.belongsTo('category')
 });
